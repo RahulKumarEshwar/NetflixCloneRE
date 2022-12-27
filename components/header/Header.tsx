bead057246d3117c260fc4c2b762da28778a25d0
RahulEshwar <rahul.eshwar@wmlit.com>
2022-12-27 17:56:19 +0530

    Update Header.tsx

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
                     <li className="headerLink">TV Shows</li>
                     <li className="headerLink">Movies</li>
                     <li className="headerLink">New & Popular</li>
-                    <li className="headerLink">My List</li>
+                    <li className="headerLink">My Account</li>
                 </ul>
             </div>
             <div className='flex items-center space-x-4 text-sm font-light'>
